f4179d0bf483c4dd2f7c1d65d305618e95b72ec6
unknown <shumbushoemilef@gmail.com>
2023-03-01 13:56:25 +0200

    chore(project structure): remove unnecessary codes and adress reviewer feedback #184581160

--- src/app.js
+++ src/app.js
@@ -1,17 +1,17 @@
-import http from "http";
 import express from "express";
 import logger from "morgan";
 import dotenv from "dotenv";
 import cors from "cors";
 
-const app = express();
-const server = http.createServer(app);
-
 dotenv.config();
 
-app.use(logger("dev"));
+const app = express();
+
+if (process.env.NODE_ENV === "development") {
+  app.use(logger("dev"));
+}
+
 app.use(express.json());
 app.use(cors());
 
-app.server = server;
 export default app;
