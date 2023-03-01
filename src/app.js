0a1e26c0803409475f916a36cf1947f237f51448
unknown <shumbushoemilef@gmail.com>
2023-03-01 13:56:25 +0200

    chore(project structure): remove unnecessary codes and adress reviewer feedback #184581160

--- src/app.js
+++ src/app.js
@@ -1,8 +1,15 @@
+<<<<<<< HEAD
 import express from 'express';
 import logger from 'morgan';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import userRoute from './routes/user';
+=======
+import express from "express";
+import logger from "morgan";
+import dotenv from "dotenv";
+import cors from "cors";
+>>>>>>> 3d20cd5 (chore(project structure): remove unnecessary codes and adress reviewer feedback #184581160)
 
 dotenv.config();
 
