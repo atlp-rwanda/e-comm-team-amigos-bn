f519c28b5f93b359c4a8726e18182202a3c80124
1MuhireDavid <39764771+1MuhireDavid@users.noreply.github.com>
2023-03-06 11:02:30 +0200

    fix(app): delete  package-lock.json file

--- src/app.js
+++ src/app.js
@@ -1,19 +1,17 @@
-import express from "express";
-import logger from "morgan";
-import dotenv from "dotenv";
-import cors from "cors";
-import swaggerUi from "swagger-ui-express";
-import specs from "./docs"
-import userRoute from "./routes/user";
-import swaggerUi from "swagger-ui-express";
-import specs from "./docs"
+import express from 'express';
+import logger from 'morgan';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import swaggerUi from 'swagger-ui-express';
+import specs from './docs';
+import userRoute from './routes/user';
+
 dotenv.config();
 
 const app = express();
 
-
-if (process.env.NODE_ENV === "development") {
-  app.use(logger("dev"));
+if (process.env.NODE_ENV === 'development') {
+  app.use(logger('dev'));
 }
 
 app.use('/user', userRoute);
@@ -21,8 +19,8 @@ app.use('/user', userRoute);
 app.use(express.json());
 app.use(cors());
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(specs));
-app.get("/", (req, res) => {
-  res.send("Hello, There! this is Amigos ecommerce team project.");
+app.get('/', (req, res) => {
+  res.send('Hello, There! this is Amigos ecommerce team project.');
 });
 
 export default app;
