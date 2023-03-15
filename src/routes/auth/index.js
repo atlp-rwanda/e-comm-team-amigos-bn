f7fa7a11fdad316f4f6da1320da71c91110436a9
1MuhireDavid <muhiredavid74@gmail.com>
2023-03-15 09:02:41 +0200

    ch(authorization): Add authorization middleware
    [Finishes #184581177]

--- src/routes/auth/index.js
+++ src/routes/auth/index.js
@@ -1,10 +1,10 @@
-import express from "express";
-import authentication from "../../controllers/authentication";
-import { loginUser } from "../../controllers/authentication";
-import validate from "../../middleware/authValidation";
+import express from 'express';
+import authentication from '../../controllers/authentication';
+import { loginUser } from '../../controllers/authentication';
+import validate from '../../middleware/authValidation';
 const router = express.Router();
-router.post("/create", validate.signUpValidator, authentication.createUser);
-router.post("/login", validate.loginValidator, loginUser);
-router.get("/verify_email/:token", authentication.emailVerification);
+router.post('/create', validate.signUpValidator, authentication.createUser);
+router.post('/login', validate.loginValidator, loginUser);
+router.get('/verify_email/:token', authentication.emailVerification);
 
 export default router;
