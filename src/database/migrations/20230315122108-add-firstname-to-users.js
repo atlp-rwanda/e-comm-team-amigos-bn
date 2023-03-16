7fe600a3af8361b66fe3f794dcebbe7bd4a8506c
1MuhireDavid <muhiredavid74@gmail.com>
2023-03-16 15:29:24 +0200

    ch(user): add authorization
    - add authorize middleware to be used on protected routes
    
    [Finishes #18451177]

---
+++ src/database/migrations/20230315122108-add-firstname-to-users.js
@@ -0,0 +1,69 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4
+      },
+      firstName: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      lastName: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      userName: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      telephone: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      address: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      role: {
+        type: Sequelize.ENUM(['admin', 'vendor', 'normal']),
+        allowNull: false,
+        defaultValue: 'normal'
+      },
+      status: {
+        type: Sequelize.ENUM(['active', 'inactive']),
+        allowNull: false,
+        defaultValue: 'inactive'
+      },
+      verified: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Users');
+  }
+};
--- src/middleware/verifyToken.js
+++ src/middleware/verifyToken.js
@@ -1,5 +1,5 @@
 import jwt from 'jsonwebtoken';
-
+import asyncHandler from 'express-async-handler';
 import dotenv from 'dotenv';
 
 dotenv.config();
