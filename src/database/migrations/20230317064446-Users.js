5facf327ff34f97b4eeda9eefa973b2626d936e8
Wilbrord <bwilbrord@gmail.com>
2023-03-23 15:38:03 +0200

    feat(Remove password): Removing User Password and Email in Response of User information - When we retrieve user information we should see all info excluding User-paasword and User-email [Delivers #184769261]

--- src/database/migrations/20230317064446-Users.js
+++ src/database/migrations/20230317064446-Users.js
@@ -1,76 +1,98 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Users', {
-      id: {
-        allowNull: false,
-        primaryKey: true,
-        type: Sequelize.UUID,
-        defaultValue: Sequelize.UUIDV4,
-      },
-      firstName: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-      lastName: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-      userName: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-      telephone: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-      address: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-      email: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-      password: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-      role: {
-        type: Sequelize.ENUM(['admin', 'vendor', 'normal']),
-        allowNull: false,
-        defaultValue: 'normal',
-      },
-      status: {
-        type: Sequelize.ENUM(['active', 'inactive']),
-        allowNull: false,
-        defaultValue: 'inactive',
-      },
-      verified: {
-        type: Sequelize.BOOLEAN,
-        allowNull: false,
-        defaultValue: false,
-      },
-      otpcode: {
-        type: Sequelize.STRING,
-        allowNull: true,
-      },
-      otpcodeexpiration: {
-        type: Sequelize.STRING,
-        allowNull: true,
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-      },
-    });
-  },
+    async up(queryInterface, Sequelize) {
+        await queryInterface.createTable('Users', {
+            id: {
+                allowNull: false,
+                primaryKey: true,
+                type: Sequelize.UUID,
+                defaultValue: Sequelize.UUIDV4,
+            },
+            firstName: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            lastName: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            address: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            userName: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            telephone: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            billingAddress: {
+                type: Sequelize.STRING,
+                allowNull: true,
+            },
+            preferredLanguage: {
+                type: Sequelize.STRING,
+                allowNull: true,
+            },
+            birthdate: {
+                type: Sequelize.DATE,
+                allowNull: true,
+            },
+            preferredCurrency: {
+                type: Sequelize.ENUM(['Frw', 'USD']),
+                allowNull: true,
+                defaultValue: 'Frw',
+            },
+            gender: {
+                type: Sequelize.ENUM(['Male', 'Female']),
+                allowNull: true,
+                defaultValue: 'Male',
+            },
+            email: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            password: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            role: {
+                type: Sequelize.ENUM(['admin', 'vendor', 'normal']),
+                allowNull: false,
+                defaultValue: 'normal',
+            },
+            status: {
+                type: Sequelize.ENUM(['active', 'inactive']),
+                allowNull: false,
+                defaultValue: 'inactive',
+            },
+            verified: {
+                type: Sequelize.BOOLEAN,
+                allowNull: false,
+                defaultValue: false,
+            },
+            otpcode: {
+                type: Sequelize.STRING,
+                allowNull: true,
+            },
+            otpcodeexpiration: {
+                type: Sequelize.STRING,
+                allowNull: true,
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+            },
+        })
+    },
 
-  async down(queryInterface) {
-    await queryInterface.dropTable('Users');
-  },
-};
+    async down(queryInterface) {
+        await queryInterface.dropTable('Users')
+    },
+}
