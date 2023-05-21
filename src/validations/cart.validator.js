1af66541b653254da75b420456cdf420cd6e9b4c
AbdoulKhaliq59 <kananuraabdulkhaliq59@gmail.com>
2023-05-21 13:22:05 +0200

    bug(fix update Cart):Fixing bug on validator of update Cart  - Resolve this functionality  [Finishes]

--- src/validations/cart.validator.js
+++ src/validations/cart.validator.js
@@ -13,21 +13,19 @@ export async function validateCartAdd(req, res, next) {
 
         const quantity = parseInt(req.body.quantity);
         if (quantity < 1 || quantity > product.quantity) {
-            
             return res.status(400).json({ error: 'Quantity not Available' });
         }
-            const cart = await models.Cart.findOne({ where: { userId } });
-            if (cart) {
-                const cartproduct = await cart.products.find(
-                    (item) => item.productId == req.body.productId
-                );
-                if (cartproduct)
-                    return res.status(400).json({
-                        error: 'Product in cart already. Please update the cart.',
-                    });
-                
-            }
-    
+        const cart = await models.Cart.findOne({ where: { userId } });
+        if (cart) {
+            const cartproduct = await cart.products.find(
+                (item) => item.productId == req.body.productId
+            );
+            if (cartproduct)
+                return res.status(400).json({
+                    error: 'Product in cart already. Please update the cart.',
+                });
+        }
+
         next();
     } catch (err) {
         res.status(400).json({ status: 'fail', message: err.message });
@@ -37,23 +35,34 @@ export async function validateCartUpdate(req, res, next) {
     try {
         const userId = req.user.id;
         const productId = req.params.id;
-        const product = await models.Product.findOne({ where: { id: productId } });
+        const product = await models.Product.findOne({
+            where: { id: productId },
+        });
         const { quantity } = req.body;
         const cart = await models.Cart.findOne({ where: { userId } });
-        if (cart) {
-            const existIteminCart = cart.products.map((product) => {
-                if (product.productId !== productId) {
-                    return res.status(404).json({
-                        message: 'Item not found in cart',
-                    });
-                }
+
+        if (!cart) {
+            return res.status(404).json({
+                message: 'Cart not found',
             });
-            if (quantity < 1 || quantity > product.quantity) {
-                return res.status(400).json({ error: 'Quantity not available' });
-            }
         }
-    next();
-} catch (err) {
-    res.status(400).json({ status: 'fail', message: err.message });
-}
+
+        const existItemInCart = cart.products.find(
+            (cartProduct) => cartProduct.productId === productId
+        );
+
+        if (!existItemInCart) {
+            return res.status(404).json({
+                message: 'Item not found in cart',
+            });
+        }
+
+        if (quantity < 1 || quantity > product.quantity) {
+            return res.status(400).json({ error: 'Quantity not available' });
+        }
+
+        next();
+    } catch (err) {
+        res.status(400).json({ status: 'fail', message: err.message });
+    }
 }
